d22d610dcdcf56c123dd17f4024ff6a95a1a6c2c
songpengshishen <songpengshishen@163.com>
2025-03-30 16:53:40 +0800

    修改代码

--- inference.py
+++ inference.py
@@ -16,10 +16,10 @@ parser = argparse.ArgumentParser(description='Train',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 parser.add_argument('--asr', type=str, default="hubert")
-parser.add_argument('--dataset', type=str, default="")  
-parser.add_argument('--audio_feat', type=str, default="")
-parser.add_argument('--save_path', type=str, default="")     # end with .mp4 please
-parser.add_argument('--checkpoint', type=str, default="")
+parser.add_argument('--dataset', type=str, default="E:\pyProject/Ultralight-Digital-Human/maochao/")
+parser.add_argument('--audio_feat', type=str, default="E:\pyProject/Ultralight-Digital-Human/maochao/aud_hu.npy")
+parser.add_argument('--save_path', type=str, default="E:\pyProject/Ultralight-Digital-Human/maochao/生成无音频数字人视频.mp4")     # end with .mp4 please
+parser.add_argument('--checkpoint', type=str, default="E:\pyProject/Ultralight-Digital-Human/maochao/checkpoint/60.pth")
 args = parser.parse_args()
 
 checkpoint = args.checkpoint
